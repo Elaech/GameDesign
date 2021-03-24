3899e1932e386a4e8bf0b7e29a56da4aa48e02e3
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-03-24 16:15:40 +0200

    Finished Bee Sprites and AI

--- DroneGame/Scripts/BumpEnemy.gd
+++ DroneGame/Scripts/BumpEnemy.gd
@@ -5,12 +5,14 @@ var life = 100
 var player_detected = false
 onready var player = get_node("../Player")
 onready var timer = get_node("DirectionTimer")
+onready var sprite = get_node("AnimatedSprite")
 var direction_time = 0.25
 var motion = Vector2.ZERO
 var direction = Vector2.ZERO
 var MAX_SPEED = 60
 var SPEED = 40
 var WANDER_SPEED = 2
+var BUMP_SPEED = 4
 var FRICTION = 0.03
 var rng = RandomNumberGenerator.new()
 
@@ -18,19 +20,30 @@ func _ready():
 	rng.randomize()
 
 func _process(_delta):
-	if player_detected:
+	if player_detected and player!=null:
 		if timer.is_stopped():
 			direction = (-self.position + player.position).normalized()
 			timer.start(direction_time)
 		motion += direction*SPEED
-	motion.x += rng.randf_range(-1,1) * WANDER_SPEED
-	motion.y += rng.randf_range(-1,1) * WANDER_SPEED
+		motion.x += rng.randf_range(-1,1) * BUMP_SPEED
+		motion.y += rng.randf_range(-1,1) * BUMP_SPEED
+		adapt_sprite()
+	else:
+		motion.x += rng.randf_range(-1,1) * WANDER_SPEED
+		motion.y += rng.randf_range(-1,1) * WANDER_SPEED
+	
 	motion.x = clamp(motion.x,-MAX_SPEED,MAX_SPEED)
 	motion.y = clamp(motion.y,-MAX_SPEED,MAX_SPEED)
 	motion.x = lerp(motion.x,0,FRICTION)
 	motion.y = lerp(motion.y,0,FRICTION)
 	motion = move_and_slide(motion)
 		
+
+func adapt_sprite():
+	if direction.x > 0.0:
+		sprite.flip_h = false
+	elif direction.x < 0.0:
+		sprite.flip_h = true
 
 func _on_Hurtbox_area_entered(area):
 	if area.has_method("get_damage"):
@@ -48,3 +61,7 @@ func _on_PlayerDetector_area_entered(_area):
 
 func _on_DirectionTimer_timeout():
 	timer.stop()
+
+
+func _on_PlayerDetector_area_exited(area):
+	player_detected = false
