88f740f13e999ec72ea5e7fdb5cd931eb1c3c9ce
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-04-02 16:51:30 +0300

    Improved Data Saving

--- DroneGame/Scripts/TitleScreen.gd
+++ DroneGame/Scripts/TitleScreen.gd
@@ -1,16 +1,29 @@
 extends Control
 
+const CC = "CC"
+const UNLOCKED_LVL = "unlocked_levels"
+const MAX_HEALTH = "max_health"
+const DAMAGE_FACTOR = "damage_factor"
+const CURRENT_CHECKPOINT = "current_checkpoint"
+const CURRENT_LEVEL = "current_level"
 const PATH_TO_SAVE = "user://save.dat"
 const PASSWORD = "10293847"
+const CURRENT_VERSION = 0
+
+
 var player_data
 onready var counter = get_node("CursedChips/Counter")
-
+onready var play_label = get_node("Buttons/PlayButton/Label")
 
 func _ready():
 	load_player_data()
 	print(player_data)
 	counter.text = str(player_data["CC"])
-
+	if(player_data[CURRENT_LEVEL] != null):
+		if(player_data[CURRENT_CHECKPOINT] != null):
+			play_label.text = "Resume"
+		else:
+			play_label.text = "Continue"
 
 
 
@@ -42,9 +55,14 @@ func load_player_data():
 func new_player_data():
 	player_data = {
 		"CC" : 0,
-		"levels" : [1],
+		"unlocked_levels" : [1],
 		"max_health" : 500,
-		"damage_factor": 1
+		"damage_factor": 1,
+		"current_checkpoint": null,
+		"current_level": null,
+		"current_health" : null,
+		"taken_resources" : null,
+		"version": 0
 	}
 	var file = File.new()
 	var error = file.open_encrypted_with_pass(PATH_TO_SAVE,File.WRITE,PASSWORD)
